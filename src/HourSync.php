e6dae2e8a4ad64de4c5f134708adb97d03389110
NixWord <lewinp@esiee.fr>
2011-03-11 16:29:34 +0100

    Adding HourSync -> loadCalendar method

--- src/HourSync.php
+++ src/HourSync.php
@@ -1,12 +1,44 @@
 <?php
 
+/**
+ * Simple tool to find free time to set up an appointment
+ * @package HourSync
+ * @author NixWord
+ *
+ */
 class HourSync {
 	private $calendars = array();
 
+	/**
+	 * Add one calendar to load
+	 * @param string $calendarURI
+	 */
 	public function addCalendarURI($calendarURI) {
 		$this->calendars[] = $calendarURI;
 	}
 
+	/**
+	 * Load calendars
+	 * @param bool $useCache
+	 * @return true on success
+	 * @throws CalendarLoaderException
+	 */
+	public function loadCalendars($useCache = true) {
+		foreach($this->calendars as $calendar) {
+			$ical = file_get_contents($calendar);
+			$hash = md5($calendar);
+			$cache = './cache/' . $hash;
+			file_put_contents($cache, $ical);
+			touch($cache);
+		}
+
+		return true;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
 	public function getCalendars() {
 		return $this->calendars;
 	}
--- tests/HourSync.php
+++ tests/HourSync.php
@@ -6,6 +6,7 @@ class HourSyncTest extends PHPUnit_Framework_TestCase {
 
 	const VALID_CALENDAR_URI = "http://localhost/calendar.ics";
 	const INVALID_CALENDAR_URI = "invalid://invalid/calendar.ics";
+	const UNREADABLE_CALENDAR_URI = "http://localhost/404/calendar.ics";
 
 	protected $hoursync;
 
@@ -13,18 +14,40 @@ class HourSyncTest extends PHPUnit_Framework_TestCase {
 		$this->hoursync = new HourSync();
 	}
 
-	public function testAddCalendarURI_Valid_URI() {
+	public function testAddCalendarURI_valid_URI() {
 		$this->hoursync->addCalendarURI(self::VALID_CALENDAR_URI);
 
 		$calendars = $this->hoursync->getCalendars();
 		$this->assertEquals($calendars[0], self::VALID_CALENDAR_URI);
 	}
 
-	public function testAddCalendarURI_Invalid_URI() {
+	public function testAddCalendarURI_invalid_URI() {
 		$this->hoursync->addCalendarURI(self::INVALID_CALENDAR_URI);
 
 		$calendars = $this->hoursync->getCalendars();
 		$this->assertEquals(count($calendars), 0);
+	}
+
+	/**
+	 * @expectedException CalendarLoaderException
+	 */
+	public function testLoadCalendars_unreadable_calendar() {
+		$this->hoursync->addCalendarURI(self::UNREADABLE_CALENDAR_URI);
+		$this->hoursync->loadCalendars();
+	}
+
+	public function testLoadCalendars_without_cache() {
+		$this->hoursync->addCalendarURI(self::VALID_CALENDAR_URI);
+		$this->hoursync->loadCalendars(false);
+	}
+
+	public function testLoadCalendars_with_cache() {
+		$this->hoursync->addCalendarURI(self::VALID_CALENDAR_URI);
+		$this->hoursync->loadCalendars();
+
+		$cache = 'src/cache/' . md5(self::VALID_CALENDAR_URI);
+
+		$this->assertFileExists($cache);
 	}
 
 	public static function tearDownAfterClass() {
