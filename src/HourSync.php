61b3d751a7c602a9910a84c5a922034d0572ca9a
NixWord <lewinp@esiee.fr>
2011-03-11 15:53:48 +0100

    Adding HourSync -> addCalendarURI method

--- src/HourSync.php
+++ src/HourSync.php
@@ -1,5 +1,13 @@
 <?php
 
 class HourSync {
+	private $calendars = array();
 
+	public function addCalendarURI($calendarURI) {
+		$this->calendars[] = $calendarURI;
+	}
+
+	public function getCalendars() {
+		return $this->calendars;
+	}
 }
--- tests/HourSync.php
+++ tests/HourSync.php
@@ -1,5 +1,32 @@
 <?php
 
+require 'src/HourSync.php';
+
 class HourSyncTest extends PHPUnit_Framework_TestCase {
 
+	const VALID_CALENDAR_URI = "http://localhost/calendar.ics";
+	const INVALID_CALENDAR_URI = "invalid://invalid/calendar.ics";
+
+	protected $hoursync;
+
+	protected function setUp() {
+		$this->hoursync = new HourSync();
+	}
+
+	public function testAddCalendarURI_Valid_URI() {
+		$this->hoursync->addCalendarURI(self::VALID_CALENDAR_URI);
+
+		$calendars = $this->hoursync->getCalendars();
+		$this->assertEquals($calendars[0], self::VALID_CALENDAR_URI);
+	}
+
+	public function testAddCalendarURI_Invalid_URI() {
+		$this->hoursync->addCalendarURI(self::INVALID_CALENDAR_URI);
+
+		$calendars = $this->hoursync->getCalendars();
+		$this->assertEquals(count($calendars), 0);
+	}
+
+	public static function tearDownAfterClass() {
+	}
 }
